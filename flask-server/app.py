9d571c9591caefd3e170e1d18391b556db0d87fb
khushal.vyas <khushal.vyas@hashedin.com>
2021-03-11 16:13:58 +0530

    integrating frontend and backend

--- flask-server/app.py
+++ flask-server/app.py
@@ -20,8 +20,20 @@ mongo = PyMongo(app)
 def my_index():
     return render_template("index.html", flask_token="Hello   world")
 
-# @app.route('/upload')
-# def upload_file1():
-#    return render_template('uploading.html')
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      print("********************************************")
+      filename = secure_filename(f.filename)
+      f.save(filename)
+      new_path = os.path.abspath(filename)
+    #   mongo.save_file(f.filename, f)
+    #   mongo.send_file()
+      data = resumeparse.read_file(filename)
+      with open("sample.json", "w") as outfile:
+        json.dump(data, outfile)
+
+   return render_template('index.html', data=data)
 
 app.run(debug=True)
---
+++ react-app/src/components/Banner.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Container, Row, Col, Button } from 'react-bootstrap';
+import styled from 'styled-components';
+import banner from './banner.jpg';
+import './banner.css';
+
+const Styles = styled.div`
+  .jumbo {
+    background: url(${banner}) no-repeat fixed bottom;
+    background-size: cover;
+    color: #efefef;
+    height: 300px;
+    position: relative;
+    z-index: -2;
+    margin-bottom: 0;
+    margin-top: 5%;
+    
+  }
+  .overlay {
+    background-color: #000;
+    opacity: 0.6;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    
+  }
+`;
+
+export const Banner = () => (
+  <Styles>
+    <Jumbo fluid className="jumbo">
+      <div className="overlay">
+        <Container className="main">
+          <div className="des1">
+            <h1>Iprofiler</h1>
+            <p>Take the hassle out,</p>
+            <p>for the remote recruitment process</p>
+          </div>
+          <div>
+            <hr className="hr-style" />
+            <p className="des2">A profile filtering tool that helps selecting</p>
+            <p className="des2">out relevant prospects from a pool of generic applications.</p>
+            <Button variant="info" className="button">Let's Hire {'>>'}</Button>{' '}
+          </div>
+        </Container>
+      </div>
+    </Jumbo>
+  </Styles>
+)
