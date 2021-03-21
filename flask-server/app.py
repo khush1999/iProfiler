f4d0af2c0519b26b29966796cfc11630bfba3746
Pratik Sabata <pratik6009sabata@gmail.com>
2021-03-21 11:31:26 +0530

    Worked on Downloading Resume

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, session, abort, jsonify, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, session, send_file
 from flask_pymongo import PyMongo
 import bcrypt
 import urllib.request
@@ -19,7 +19,6 @@ app.config['MONGO_URI'] = 'mongodb+srv://codekhal:khushal11@mycluster.omgad.mong
 mongo = PyMongo(app)
 db = mongo
 filename = ""
-app.config['UPLOAD_FOLDER'] = '../react-app/src/resumes/'
 
 
 @app.route("/")
@@ -35,33 +34,28 @@ def upload_file():
         print("********************************************")
         # fileName = str(uuid.uuid4())[:8]
         filename = secure_filename(f.filename)
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        f.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
+        f.save(filename)
         print(f.filename)
         print(filename)
+        # new_path = os.path.abspath(filename)
         filenam = f.filename
         mongo.save_file(filename, f)
         #   mongo.send_file()
         data = resumeparse.read_file(filename)
         data["resume_id"] = filename
-        # print(data)
-        # with open(os.path.join("static/resumes", filename), "wb") as fp:
-        #     fp.write(request.data)
-
+        print(data)
         with open("../react-app/src/sample.json", "w") as outfile:
             json.dump(data, outfile)
     return data
 
-# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-# https://medium.com/excited-developers/file-upload-with-react-flask-e115e6f2bf99
 
-# @app.route("/upload/<path>")
-# def get_file(path=None):
-#     if path is not None:
-#         print("Download a file......")
-#         return send_from_directory(app.config['UPLOAD_URI'], path)
-#     else:
-#         print("Sorryyyyyyyyyyyyyyyyyy!")
+@app.route("/getData/<path>")
+def get_file(path=None):
+    if path is not None:
+        print("Download a file......")
+        return send_file(path)
+    else:
+        print("Sorryyyyyyyyyyyyyyyyyy!")
 
 
 @app.route('/getData', methods=['GET', 'POST'])
@@ -72,7 +66,7 @@ def form_files():
         a['_id'] = str(a['_id'])
         result.append(a)
     res = json.dumps(result)
-    # print(result)
+    print(result)
     return res
 
 
@@ -80,8 +74,7 @@ def form_files():
 def create():
     global data
     if request.method == 'POST':
-        print("Came inside ***************************")
-        print(request.form.get('skills1'))
+        print("Came inside", request.form.get('skills1'))
         mongo.db.users.insert_one({
             'fname': request.form.get('fname'),
             'lname': request.form.get('lname'),
@@ -105,11 +98,11 @@ def create():
             'skills3': request.form.get('skills3'),
             'total_exp': request.form.get('total_exp'),
             'designition': request.form.get('desig'),
-            'Companies worked at': request.form.get('Companies_worked_at'),
+            'Companies worked at': request.form.get('Companies worked at'),
             'resume_id': request.form.get('resume_id'),
 
         })
-        # print(list(mongo.db.users.find()))
+        print(list(mongo.db.users.find()))
     return """ <h2> We have received your response , you can now close this window!! </h2> """
 
 
@@ -131,7 +124,6 @@ def index():
         password2 = request.form.get("password2")
         # if found in database showcase that it's found
         user_found = mongo.db.LoginAuth.find_one({"company_name": user})
-        print(user_found)
         email_found = mongo.db.LoginAuth.find_one({"email": email})
         # if user_found:
         #     message = 'There already is a user by that name'
