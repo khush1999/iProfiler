65074d1848cb2b6875cf7e8a2b5bc992b2c0dfa8
Pratik Sabata <pratik.sabata@hashedin.com>
2021-03-29 10:53:47 +0000

    Update IncommingRounds.tsx

--- react-app/src/components/IncommingRounds.tsx
+++ react-app/src/components/IncommingRounds.tsx
@@ -62,7 +62,7 @@ export default function IncommingRounds(props: { location: { state: IForm } }) {
           // fs.writeFile('status.json', status, (err) => {
           //   if (err) throw err;
           // })
-          const res = await axios.get(`http://127.0.0.1:5000/create/` + email);
+          const res = await axios.get(`/create/` + email);
           console.log(res.data);
         },
         (error) => {
