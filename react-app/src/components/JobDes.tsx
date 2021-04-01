f69974259ebee03205a7a4a0ed1411d3c31db0d8
Khushal Vyas <khushal.vyas@hashedin.com>
2021-04-01 19:30:15 +0000

    Update JobDes.tsx

--- react-app/src/components/JobDes.tsx
+++ react-app/src/components/JobDes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import './JobDes.css';
-import jobhunt from '../assets/jobhunt.png';
 
 export const JobDes = () => {
     return (
