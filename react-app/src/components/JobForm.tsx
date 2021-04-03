31639fe9a34ab8ca0a445787e4a1afc7beb20d3e
astha.dwivedi <astha.dwivedi@hashedin.com>
2021-04-03 19:03:33 +0530

    Worked on job form, modal and job des components

--- react-app/src/components/JobForm.tsx
+++ react-app/src/components/JobForm.tsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
 import nextId from "react-id-generator";
 
-
+/* Structure for Modal props */
 interface IProps {
     show: boolean;
     handleClose: () => void;
 }
 
+/* This component allows HR to create a job post */
 export const JobForm = ({
     show,
     handleClose
@@ -15,7 +16,7 @@ export const JobForm = ({
 
     let JobId = nextId("job-id-");
 
-    // Form Validation:
+    // Form Validation
     const [validated, setValidated] = useState(false);
     const handleSubmit1 = (event) => {
         const form = event.currentTarget;
@@ -32,6 +33,7 @@ export const JobForm = ({
                 <Modal.Header closeButton>
                     <Modal.Title>Post a Job</Modal.Title>
                 </Modal.Header>
+
                 <Modal.Body>
                     <Form
                         noValidate
@@ -54,6 +56,7 @@ export const JobForm = ({
                                     value={JobId}
                                 />
                             </Form.Group>
+
                             <Form.Group as={Col} controlId="job-role">
                                 <Form.Label>Job Role</Form.Label>
                                 <Form.Control
@@ -87,6 +90,7 @@ export const JobForm = ({
                             Please Enter Job offer.{" "}
                                 </Form.Control.Feedback>
                             </Form.Group>
+
                             <Form.Group as={Col} controlId="job-ctc">
                                 <Form.Label>CTC(in LPA)</Form.Label>
                                 <Form.Control
@@ -120,6 +124,7 @@ export const JobForm = ({
                             Please Enter Skill.{" "}
                                 </Form.Control.Feedback>
                             </Form.Group>
+
                             <Form.Group as={Col} controlId="job-skill2">
                                 <Form.Label>Skill 2</Form.Label>
                                 <Form.Control
@@ -135,6 +140,7 @@ export const JobForm = ({
                             Please Enter Skill.{" "}
                                 </Form.Control.Feedback>
                             </Form.Group>
+
                             <Form.Group as={Col} controlId="job-skill3">
                                 <Form.Label>Skill 3</Form.Label>
                                 <Form.Control
@@ -151,6 +157,7 @@ export const JobForm = ({
                                 </Form.Control.Feedback>
                             </Form.Group>
                         </Form.Row>
+                        
                         <Form.Row>
                         <Form.Group as={Col} controlId="job-des">
                                 <Form.Label>Job Description</Form.Label>
@@ -167,6 +174,7 @@ export const JobForm = ({
                                 </Form.Control.Feedback>
                             </Form.Group>
                         </Form.Row>
+
                         <Modal.Footer>
                             <Form.Group as={Row}>
                                 <Col sm={{ span: 10, offset: 2 }}>
@@ -180,6 +188,7 @@ export const JobForm = ({
                                 </Col>
                             </Form.Group>
                         </Modal.Footer>
+                        
                     </Form>
                 </Modal.Body>
             </Modal>
