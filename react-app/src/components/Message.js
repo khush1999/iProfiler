d05cd5eb9c970a65247e8f023a852f0510ac1960
astha.dwivedi <astha.dwivedi@hashedin.com>
2021-04-03 19:08:10 +0530

    Worked on navdashboard and message and progress bar

--- react-app/src/components/Message.js
+++ react-app/src/components/Message.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/* This component handles the status of file upload */
 const Message = ({ msg }) => {
   return (
     <div className='alert alert-info alert-dismissible fade show mb-3' role='alert'>
